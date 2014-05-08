5c50a05cea9047dd5385dd9f9af53a9d6b875102
Robin <robindoble@gmail.com>
2014-05-08 12:25:06 +0100

    created rspec environment, added datamapper, passed one test on the database for the link class/table

---
+++ Gemfile
@@ -0,0 +1,16 @@
+source 'https://rubygems.org'
+
+gem 'sinatra'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
+
+
+group :development, :test do 
+
+gem 'cucumber'
+gem 'capybara'
+gem 'rspec'
+gem 'shotgun'
+gem 'database_cleaner'
+
+end
---
+++ server.rb
@@ -0,0 +1,16 @@
+require 'sinatra/base'
+require 'data_mapper'
+
+
+env = ENV["RACK_ENV"] || "development"
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+
+require './lib/link'
+
+DataMapper.finalize
+DataMapper.auto_upgrade!
+
+class MyApp < Sinatra::Base
+
+
+end
