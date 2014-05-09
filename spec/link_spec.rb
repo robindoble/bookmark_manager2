1336c56dd8c2386b0a0f5bae332f15cd91dc808e
Robin <robindoble@gmail.com>
2014-05-09 15:50:02 +0100

    rspec passed to demonstrate that links can have multiple tags and Datampper has added them to another resource

--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -1,9 +1,10 @@
 require 'spec_helper'
-# requrie 'link'
+
 
 describe Link do
 
 	context 'demo of how datamapper works' do			
+	
 			it 'should be created and then retrieved from the DB' do 
 			expect(Link.count).to eq(0)
 			Link.create(:title => "Makers Academy", :url => "http://www.makersacademy.com/")
@@ -14,9 +15,19 @@ describe Link do
 			link.destroy
 			expect(Link.count).to eq(0)
 			end
+					
+			it 'can have tags' do 
+# 
+			tag_strings = "codeschool","education"
+			tag_array = tag_strings.to_a.map {|tag| Tag.first_or_create(:tag_name => tag)}
+			link = Link.create(:title => "Makers Academy", :url => "http://www.makersacademy.com/", :tags => tag_array)
+			# :text => lambda {|tag| tag.tag_name} 
+			expect(link.tags.map{|tag| tag.tag_name}).to include "codeschool"
+			expect(link.tags.map{|tag| tag.tag_name}).to include "education"
+			end
+	
 	end
-
-	end
+end
 
 
 
--- spec/tag_spec.rb
+++ spec/tag_spec.rb
@@ -1,20 +1,20 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-describe Tag do 
+# describe Tag do 
 
-	context 'a demo of how tags work' do  
+# 	context 'a demo of how tags work' do  
 
-		it 'should be created and then retrieved from the DB' do 
-			expect(Tag.count).to eq(0)
-			Tag.create(:tags => "education")
-			expect(Tag.count).to eq(1)
-			tag=Tag.first
-			expect(tag.tags).to eq("education")
-			tag.destroy
-			expect(Tag.count).to eq(0)
-		end
+# 		it 'should be created and then retrieved from the DB' do 
+# 			expect(Tag.count).to eq(0)
+# 			Tag.create(:tags => "education")
+# 			expect(Tag.count).to eq(1)
+# 			tag=Tag.first
+# 			expect(tag.tags).to eq("education")
+# 			tag.destroy
+# 			expect(Tag.count).to eq(0)
+# 		end
 
 
-	end
+# 	end
 
-end
+# end
